214c47bd73ad5337653be4430b7f7d9a8de9acab
Jagdeep009 <jagdeep.mahala27@gmail.com>
2024-11-15 13:07:28 +0530

    utils for async handler wrapper

--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,6 @@ const connectDB = async () => {
     try{
         const connecttionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         console.log("Connection Established");
-
         console.log(connecttionInstance.connection.host);
     }
     catch(error) {
